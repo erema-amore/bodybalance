17fef381fa30609c43aee6eebe695015d5fa04a7
erema-amore <erema.amore@mail.ru>
2023-09-07 15:24:31 +0600

    design of login

--- src/components/users/Login.jsx
+++ src/components/users/Login.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../../store/users/userAction';
 import { clearStatusState, clearUserState } from '../../store/users/userSlice';
 import { useNavigate } from 'react-router-dom';
-
+import style from '../../style/login.module.css'; 
 
 const Login = () => {
     const [user, setUser] = useState({
@@ -20,51 +20,133 @@ const Login = () => {
     useEffect(() => {
         dispatch(clearStatusState);
         dispatch(clearUserState);
-}, []);
+    }, []);
 
-
-return (
-  <>
-    { loading ? (
-    <Box style={{ display: "flex" }}><CircularProgress />
-    </Box>
-    ) : (
-     <>
-      { status ? (
-         <>
-           {status === "error" && (
-            <>
-              <div >
-              <div >
-                <h3>An error occured!</h3>
-                <button onClick={() => dispatch(clearStatusState()) }> Try again! </button>
-              </div>
-              </div>
-            </>
-            )}
-             </>
+    return (
+        <>
+            {loading ? (
+                <Box style={{ display: "flex" }}><CircularProgress />
+                </Box>
             ) : (
-            <>
-            <div >
-            <div >login form</div>
-             </div>
-            <div >
-            <div >
-            <div >
-              <input type="text" placeholder="Username" onChange={(e) =>
-              setUser({...user, username: e.target.value }) } />
-              <input type="password" placeholder="Password" onChange={(e) =>
-              setUser({...user, password: e.target.value, }) } />
-              <button onClick={() => dispatch(loginUser({user, navigate}))} >Login</button>
-            </div>
-            </div>
-            </div>
-             </>
-               ) }
-               </>
-            ) }
+                <>
+                    {status ? (
+                        <>
+                            {status === "error" && (
+                                <>
+                                    <div className={style.errorContainer}>
+                                        <div className={style.errorText}>
+                                            <h3>An error occurred!</h3>
+                                            <button className={style.errorButton} onClick={() => dispatch(clearStatusState())}> Try again! </button>
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            <div className={style.loginContainer}>
+                                <div className={style.formContainer}></div>
+                                <div>
+                                    <div className={style.inputMain}>
+                                        <input
+                                            className={style.inputField}
+                                            type="text"
+                                            placeholder="Username"
+                                            onChange={(e) => setUser({ ...user, username: e.target.value })}
+                                        />
+                                        <input
+                                            className={style.inputField}
+                                            type="password"
+                                            placeholder="Password"
+                                            onChange={(e) => setUser({ ...user, password: e.target.value, })}
+                                        />
+                                        <button className={style.loginButton} onClick={() => dispatch(loginUser({ user, navigate }))}>Login</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
         </>
     );
 };
 
 export default Login;
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { CircularProgress, Box } from '@mui/material';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { loginUser } from '../../store/users/userAction';
+// import { clearStatusState, clearUserState } from '../../store/users/userSlice';
+// import { useNavigate } from 'react-router-dom';
+// import '../../style/login.module.css'
+
+
+
+// const Login = () => {
+//     const [user, setUser] = useState({
+//         username: "",
+//         password: "",
+//     });
+
+//     const { loading, status } = useSelector(state => state.user);
+
+//     const navigate = useNavigate();
+//     const dispatch = useDispatch();
+
+//     useEffect(() => {
+//         dispatch(clearStatusState);
+//         dispatch(clearUserState);
+// }, []);
+
+
+// return (
+//   <>
+//     { loading ? (
+//     <Box style={{ display: "flex" }}><CircularProgress />
+//     </Box>
+//     ) : (
+//      <>
+//       { status ? (
+//          <>
+//            {status === "error" && (
+//             <>
+//               <div >
+//               <div >
+//                 <h3>An error occured!</h3>
+//                 <button onClick={() => dispatch(clearStatusState()) }> Try again! </button>
+//               </div>
+//               </div>
+//             </>
+//             )}
+//              </>
+//             ) : (
+//             <>
+//             <div >
+//             <div >login form</div>
+//              </div>
+//             <div >
+//             <div >
+//             <div >
+//               <input type="text" placeholder="Username" onChange={(e) =>
+//               setUser({...user, username: e.target.value }) } />
+//               <input type="password" placeholder="Password" onChange={(e) =>
+//               setUser({...user, password: e.target.value, }) } />
+//               <button onClick={() => dispatch(loginUser({user, navigate}))} >Login</button>
+//             </div>
+//             </div>
+//             </div>
+//              </>
+//                ) }
+//                </>
+//             ) }
+//         </>
+//     );
+// };
+
+// export default Login;
--- src/components/users/Register.jsx
+++ src/components/users/Register.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
@@ -5,11 +6,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { registerUser } from "../../store/users/userAction";
 import { clearStatusState } from "../../store/users/userSlice";
 import { useNavigate } from "react-router-dom";
+import style from '../../style/register.module.css'
 
 const Register = () => {
   const [user, setUser] = useState({
-      username: "",
-      password: "",
+    username: "",
+    password: "",
   });
 
   const { loading, status } = useSelector((state) => state.user);
@@ -18,56 +20,135 @@ const Register = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-      dispatch(clearStatusState());
+    dispatch(clearStatusState());
   }, []);
 
   return (
-      <>
+    <div className={style.registerContainer}>
       {loading ? (
-        <>
-          <Box style={{ display: "flex" }}><CircularProgress />
-          </Box>
-        </>
-          ) : (
+        <Box style={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      ) : (
         <>
           {status ? (
-        <>
-           {status === "error" && (
             <>
-              <div >
-                <div >
-                  <h3>An error occured!</h3>
-                   <button onClick={() => dispatch(clearStatusState()) }> Try again!</button>
+              {status === "error" && (
+                <div className={style.errorContainer}>
+                  <div className={style.errorText}>
+                    <h3>An error occurred!</h3>
+                    <button className={style.errorButton} onClick={() => dispatch(clearStatusState())}> Try again!</button>
+                  </div>
+                </div>
+              )}
+            </>
+          ) : (
+            <>
+              <div className={style.formContainer}>
+                {/* Register form */}
+              </div>
+              <div className={style.formContainer}>
+                <div className={style.inputFieldContainer}>
+                  <input
+                    className={style.inputField}
+                    type="text"
+                    placeholder="Username"
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
+                  />
+                  <input
+                    className={style.inputField}
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                  />
+                  <button className={style.registerButton} onClick={() => dispatch(registerUser({ user, navigate }))}> Register </button>
                 </div>
               </div>
             </>
-             )}
-         </>
-            ) : (
-           <>
-            <div >
-              <div >
-                Register form
-              </div>
-            </div>
-
-            <div >
-               <div >               
-                <div >
-                  <input type="text" placeholder="Username" onChange={(e) => 
-                   setUser({...user,username: e.target.value,}) } />
-                  <input type="password"placeholder="Password"onChange={(e) =>
-                    setUser({...user, password: e.target.value, }) } />
-                  <button onClick={() =>dispatch( registerUser({user,navigate, }))}> Register </button>
-                   </div>
-                </div>
-            </div>
-           </>
-                  )}
-              </>
           )}
-      </>
+        </>
+      )}
+    </div>
   );
 };
 
 export default Register;
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import CircularProgress from "@mui/material/CircularProgress";
+// import Box from "@mui/material/Box";
+// import { useDispatch, useSelector } from "react-redux";
+// import { registerUser } from "../../store/users/userAction";
+// import { clearStatusState } from "../../store/users/userSlice";
+// import { useNavigate } from "react-router-dom";
+
+// const Register = () => {
+//   const [user, setUser] = useState({
+//       username: "",
+//       password: "",
+//   });
+
+//   const { loading, status } = useSelector((state) => state.user);
+
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//       dispatch(clearStatusState());
+//   }, []);
+
+//   return (
+//       <>
+//       {loading ? (
+//         <>
+//           <Box style={{ display: "flex" }}><CircularProgress />
+//           </Box>
+//         </>
+//           ) : (
+//         <>
+//           {status ? (
+//         <>
+//            {status === "error" && (
+//             <>
+//               <div >
+//                 <div >
+//                   <h3>An error occured!</h3>
+//                    <button onClick={() => dispatch(clearStatusState()) }> Try again!</button>
+//                 </div>
+//               </div>
+//             </>
+//              )}
+//          </>
+//             ) : (
+//            <>
+//             <div >
+//               <div >
+//                 Register form
+//               </div>
+//             </div>
+
+//             <div >
+//                <div >               
+//                 <div >
+//                   <input type="text" placeholder="Username" onChange={(e) => 
+//                    setUser({...user,username: e.target.value,}) } />
+//                   <input type="password"placeholder="Password"onChange={(e) =>
+//                     setUser({...user, password: e.target.value, }) } />
+//                   <button onClick={() =>dispatch( registerUser({user,navigate, }))}> Register </button>
+//                    </div>
+//                 </div>
+//             </div>
+//            </>
+//                   )}
+//               </>
+//           )}
+//       </>
+//   );
+// };
+
+// export default Register;
